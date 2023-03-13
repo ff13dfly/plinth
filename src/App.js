efdb52a18d5db68fab2361c2aeac94d28d352c9c
fuu <fu@yihaofang.com>
2023-03-13 21:35:17 +0800

    React framework.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+import { useState,useEffect,useCallback} from 'react';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { Keyring } from '@polkadot/api';
+
+function App() {
+  return (<div id="container"></div>);
+}
+
+export default App;
