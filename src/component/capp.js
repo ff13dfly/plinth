1c5445f7b06d91b86ee00768524777148401026b
fuu <fu@yihaofang.com>
2023-05-04 22:58:08 +0800

    cApp struct improvement.

--- src/component/capp.js
+++ src/component/capp.js
@@ -1,5 +1,7 @@
 //import { Row, Col } from 'react-bootstrap';
 import { useState,useEffect} from 'react';
+import { anchorJS } from "../lib/anchor";
+import { easyRun } from "../lib/easy";
 
 function CApp(props) {
   let [info,setInfo]=useState(props.loading?props.loading:"Loading...");
@@ -7,29 +9,36 @@ function CApp(props) {
   const easy=props.easy;
 
   //params to cApp
-  const cfg={
+  const input={
     container:props.id,
     from:null,
     params:{},
   };
 
   const APIs={
-
+    anchorJS:anchorJS,
+    easy:easyRun,
   }
   
   //console.log(props.id);
+  //FIXME, lib of JS need to `eval`
 
   useEffect(() => {
     console.log(easy);
     try {
-      const cApp = new Function("API", "config", "errs", easy.code);
+      const pa='API',pb='input',pc='errs';
+
+      //!important, closure function to isolate the namespace.
+      //!important
+      const str=`;(function(${pa},${pb},${pc}){${easy.code}})(${pa},${pb},${pc})`;
+      const cApp = new Function(pa, pb, pc,str);
       if (cApp){
-        //console.log(cApp);
         setInfo("");
-        cApp(APIs,cfg, []);
+        cApp(APIs,input, easy.error);
       }
     } catch (error) {
-      setInfo(JSON.stringify(error));
+      console.log(error);
+      setInfo(JSON.stringify({error}));
     }
   }, []);
 
@@ -37,9 +46,7 @@ function CApp(props) {
     <div>
       <style>{easy.libs && easy.libs.css?easy.libs.css:''}</style>
       <script>{easy.libs && easy.libs.js?easy.libs.js:''}</script>
-      <div id={props.id}>
-        {info}
-      </div>
+      <div id={props.id}>{info}</div>
     </div>
   );
 }
